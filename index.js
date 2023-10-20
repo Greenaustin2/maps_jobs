e8ea11634f062b32e15f76b63b7c26b189e68a91
Greenaustin2 <greenaustin2@gmail.com>
2023-10-19 21:22:32 -0400

    web scraping integration

--- index.js
+++ index.js
@@ -1,118 +1,98 @@
-const API =
-  "https://maps.googleapis.com/maps/api/place/textsearch/json?query=bar&key=AIzaSyASuJcv2I7IQEN31z6fnFa0SYn1v4SOgic&latitude=40.713012&longitude=-73.961638&radius=6500";
+import "dotenv/config";
+import * as cheerio from "cheerio";
+import { default as axios } from "axios";
+
 let token;
+const apiKey = process.env.API_KEY;
+
 var x = 0;
 
+const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
+
 async function logBars() {
-  const response = await fetch(API);
+  var latLong = "40.712132, -73.962143";
+  var miles = 1;
+
+  var radius = (miles * 1609).toString();
+  var latitude = latLong.split(",")[0];
+  var longitude = latLong.split(",")[1].slice(1);
+  // var closingTime = "2";
+
+  const api = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=bar&key=${apiKey}&location=${latitude}%2C${longitude}&radius=${radius}`;
+  // console.log(api);
+  const response = await fetch(api);
   const bars = await response.json();
+  console.log(await bars);
+
   for (let i = 0; i < bars["results"].length - 1; i++) {
-    await logHours(bars["results"][i]);
+    await logHours(await bars["results"][i]);
   }
-  token = bars.next_page_token;
+  token = await bars.next_page_token;
+  await sleep(2000);
   const tokenResponse = await fetch(
-    `https://maps.googleapis.com/maps/api/place/textsearch/json?key=AIzaSyASuJcv2I7IQEN31z6fnFa0SYn1v4SOgic&pagetoken=${token}`
+    `https://maps.googleapis.com/maps/api/place/textsearch/json?key=${apiKey}&pagetoken=${token}`
   );
   const bars2 = await tokenResponse.json();
+
   for (let i = 0; i < bars2["results"].length - 1; i++) {
-    await logHours(bars2["results"][i]);
+    await logHours(await bars2["results"][i]);
   }
-  token = bars2.next_page_token;
+  token = await bars2.next_page_token;
+  await sleep(2000);
   const tokenResponse2 = await fetch(
-    `https://maps.googleapis.com/maps/api/place/textsearch/json?key=AIzaSyASuJcv2I7IQEN31z6fnFa0SYn1v4SOgic&pagetoken=${token}`
+    `https://maps.googleapis.com/maps/api/place/textsearch/json?key=${apiKey}&pagetoken=${token}`
   );
   const bars3 = await tokenResponse2.json();
+
   for (let i = 0; i < bars3["results"].length - 1; i++) {
-    await logHours(bars3["results"][i]);
+    await logHours(await bars3["results"][i]);
   }
 }
 
-// const consoleLog = (bars) => {
-//   console.log(x);
-//   for (let i = 0; i < bars["results"].length - 1; i++) {
-//     logHours(bars["results"][i]);
-//   }
-//   x += 1;
-// };
-
-// async function nextPageToken(token) {
-//   const tokenResponse = await fetch(
-//     `https://maps.googleapis.com/maps/api/place/textsearch/json?key=AIzaSyASuJcv2I7IQEN31z6fnFa0SYn1v4SOgic&pagetoken=${token}`
-//   );
-//   const bars = await tokenResponse.json();
-//   // console.log(bars);
-//   consoleLog(bars);
-//   token = bars.next_page_token;
-//   const tokenResponse2 = await fetch(
-//     `https://maps.googleapis.com/maps/api/place/textsearch/json?key=AIzaSyASuJcv2I7IQEN31z6fnFa0SYn1v4SOgic&pagetoken=${token}`
-//   );
-//   const bars2 = await tokenResponse2.json();
-//   consoleLog(bars2);
-// }
-
 async function logHours(bar) {
-  var apiUrl = `https://maps.googleapis.com/maps/api/place/details/json?fields=opening_hours&place_id=${bar["place_id"]}&key=AIzaSyASuJcv2I7IQEN31z6fnFa0SYn1v4SOgic`;
+  // console.log(bar);
+  var apiUrl = `https://maps.googleapis.com/maps/api/place/details/json?fields=opening_hours,website&place_id=${bar["place_id"]}&key=AIzaSyASuJcv2I7IQEN31z6fnFa0SYn1v4SOgic`;
   const response = await fetch(apiUrl);
   const barHours = await response.json();
   var name = bar["name"];
   var hours = await barHours;
-  if (hours.result.opening_hours.weekday_text[5] === "Closed") {
-    var saturdayClose = "closed";
+  console.log(hours.result.website);
+  scrape(hours.result.website);
+  //take saturday closing time and reduce from timecode string to single integer
+  if (!hours.result.opening_hours) {
+    var saturdayClose = "no hours";
   } else {
     var saturdayHours = hours.result.opening_hours.weekday_text[5];
+
     var saturdayClose = saturdayHours.substring(
       saturdayHours.length - 8,
       saturdayHours.length
     );
+    var closingInteger = parseInt(saturdayClose.split(/:| /)[0]);
   }
-  console.log(name + " " + saturdayClose);
+  // check if hours available
+  if (saturdayClose === "no hours") {
+    console.log(name + " " + saturdayClose);
+    // check if closed saturdays
+  } else if (hours.result.opening_hours.weekday_text[5] === "Closed") {
+    var saturdayClose = "closed";
+    console.log(name + " " + saturdayClose);
+    //filter for specified time
+  } else if (
+    (closingInteger >= 1 && closingInteger <= 5) ||
+    (closingInteger >= 9 && closingInteger <= 12)
+  ) {
+    // console.log(closingInteger);
+    console.log(name + " " + saturdayClose);
+  }
 }
 
-logBars();
+async function scrape() {
+  const webPage = await axios.get("https://radegasthall.com/");
+  const $ = cheerio.load(webPage);
+  const emailAddress = $("a").find();
+  console.log(emailAddress._root.children);
+}
 
-// import pkg from "@googlemaps/js-api-loader";
-// const { Loader } = pkg;
-// const loader = new Loader({
-//   apiKey: "AIzaSyASuJcv2I7IQEN31z6fnFa0SYn1v4SOgic",
-//   version: "weekly",
-// });
-
-// let map;
-// var service;
-// // var infowindow;
-
-// loader.importLibrary().then(async () => {
-//   const { Places } = await google.maps.importLibrary("places");
-
-// //   map = new Map(document.getElementById("map"), {
-// //     center: { lat: -34.397, lng: 150.644 },
-// //     zoom: 8,
-// //   });
-
-// var pyrmont = new google.maps.LatLng(-33.8665433,151.1956316);
-
-// map = new google.maps.Map(document.getElementById('map'), {
-//     center: pyrmont,
-//     zoom: 15
-//   });
-
-// var request = {
-//   location: pyrmont,
-//   radius: '6500',
-//   query: 'bar'
-// };
-
-// service = new google.maps.places.PlacesService(map);
-// service.textSearch(request, callback);
-
-// });
-
-// function callback(results, status) {
-//     if (status == google.maps.places.PlacesServiceStatus.OK) {
-//       for (var i = 0; i < results.length; i++) {
-//         var place = results[i];
-//         // createMarker(results[i]);
-//         // console.log(place)
-//       }
-//     }
-//     }
+// logBars();
+scrape();
